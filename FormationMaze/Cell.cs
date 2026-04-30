6c05f2b56a47fe4bef142fb08973955262119cda
Formation <formation@intm.com>
2026-04-30 16:12:16 +0200

    Debut maze

---
+++ FormationMaze/Cell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationMaze
+{
+    public struct Cell
+    {
+        // 0 : Haut, 1 : bas, 2 : gauche, 3 : droite
+        public bool[] Walls { get; set; }
+
+        public bool IsVisited { get; set; }
+
+        // Définir système d'état de la cellule
+
+        public Cell(bool mahcin = true)
+        {
+            this.IsVisited = false;
+            Walls = new bool[4];
+            Walls[0] = false;
+            Walls[1] = false;
+            Walls[2] = false;
+            Walls[3] = false;
+        }
+    }
+}
---
+++ FormationMaze/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationMaze
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nAppuie sur une touche pour finir");
+            Console.ReadKey();
+        }
+    }
+}
